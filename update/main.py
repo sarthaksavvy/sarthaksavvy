73499847177ff1161ca8995ed23078fb81d7ac62
sarthaksavvy <bitfumes@gmail.com>
2021-05-14 10:51:36 +0530

    using python to hit youtube api

--- update/main.py
+++ update/main.py
@@ -1,23 +1,28 @@
 import json
 import sys
 from datetime import datetime
+import requests
+import os
 
 
 def update():
-    with open('update/template.md', 'r') as file:
-        filedata = file.read()
+    response = requests.get(
+        f"https://www.googleapis.com/youtube/v3/search?key={os.environ['YOUTUBE_API_KEY']}&channelId={os.environ['CHANNEL_ID']}&part=id&order=date&maxResults=3")
 
-        if(len(sys.argv) > 1):
-            yt = json.loads(sys.argv[1])
+    if response.status_code == 200:
+        yt = response.json()
+        with open('update/template.md', 'r') as file:
+            filedata = file.read()
+
             filedata = filedata.replace('{{VIDEO_1_LINK}}', yt['items'][0]['id']['videoId']).replace(
                 '{{VIDEO_2_LINK}}', yt['items'][1]['id']['videoId']).replace('{{VIDEO_3_LINK}}', yt['items'][2]['id']['videoId'])
 
             filedata = filedata.replace(
                 '{{LAST_UPDATED_AT}}', datetime.now().strftime("%d %b %Y %H:%M"))
 
-        # Write the file out again
-        with open('README.md', 'w') as file:
-            file.write(filedata)
+            # Write the file out again
+            with open('README.md', 'w') as file:
+                file.write(filedata)
 
 
 def formatTime(item):
